ad87c05f8e42ca62c33073f83daca4fa6a4c30dd
fitrirahmiimami <ipitt.2303@gmail.com>
2023-07-09 21:13:58 -0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("scale-y-0");
+  navMenu.classList.toggle("overflow-hidden");
+});
